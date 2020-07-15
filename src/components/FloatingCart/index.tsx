839b8177a2a5d6f662b850dc133250f321ee95b1
dth <dth@dthlabs.com>
2020-07-15 22:30:12 +0200

    totalItemsInCart and totalcart in floatingCart

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -23,16 +23,27 @@ const FloatingCart: React.FC = () => {
 
   const navigation = useNavigation();
 
-  const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
 
-    return formatValue(0);
+  const cartTotal = useMemo(() => {
+    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsQuantity = product.quantity;
+
+      return accumulator + productsQuantity;
+    }, 0);
+
+    return formatValue(total);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
     // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
+    const total = products.reduce((accumulator, product) => {
+      const productsSubtotal = product.price * product.quantity;
 
-    return 0;
+      return accumulator + productsSubtotal;
+    }, 0);
+
+    return total;
   }, [products]);
 
   return (
